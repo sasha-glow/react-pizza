17aaf7e66ee280e8e5b453cd29452ea74ba7e895
Sasha_Glow <sasha_glow@yandex.ru>
2025-08-13 19:11:32 +0200

    feat: wip: finished lesson 6: add mockAPI, begins to learn useEffect hook, add pizzaImg in project

--- src/App.js
+++ src/App.js
@@ -7,23 +7,39 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
+// Hooks
+import { useState } from "react";
+import { useEffect } from "react";
+
 // Mocks
 import pizzaItems from './assets/pizzas.json'
 
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.mockfly.dev/mocks/3671a684-7221-435a-bec8-c83e01fe27d2/pizzas')
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        setPizzas(json)
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzaItems.map(obj => (
+            {pizzas.map(obj => (
               <PizzaBlock
                 key={obj.id}
                 {...obj}
